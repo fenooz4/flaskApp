974b94c1795fa5305b955ddd430ce3c6db056305
Franco Narvaez <franco.e.n12@gmail.com>
2022-04-18 22:17:15 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
+
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 
@@ -11,27 +12,22 @@ app.permanent_session_lifetime = timedelta(minutes=5)
 db = SQLAlchemy(app)
 
 class users(db.Model):
-
     _id = db.Column("id", db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
+    name = db.Column("name", db.String(100))
+    email = db.Column("email", db.String(100))
 
     def __init__(self, name, email):
         self.name = name
         self.email = email
 
 
-def init_db():
-    db.create_all()
-
-
-
-
-
-
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/view")
+def view():
+    return render_template("view.html", values = users.query.all())
 
 
 @app.route("/login", methods=["POST", "GET"])
@@ -89,7 +85,10 @@ def logout():
     return redirect(url_for("login"))
 
 
-if __name__ == "__main__":
-    init_db()
-
+if __name__ == '__main__':
+    db.create_all()
+    print("created all")
+    db.session.commit()
     app.run(debug=True)
+
+
